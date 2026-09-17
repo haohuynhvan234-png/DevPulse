ea7867f5c7079af955bb92eb6575711ba998ba0f
haohuynhvan234-png <haohuynhvan234@gmail.com>
2026-09-17 14:17:58 +0700

    cors

--- src/server.js
+++ src/server.js
@@ -42,3 +42,35 @@ app.listen(PORT, () => {
   console.log(`🚀 Server đang chạy tại: http://localhost:${PORT}`);
   console.log(`📚 Tài liệu Swagger UI: http://localhost:${PORT}/api-docs`);
 });
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+
+  // Vercel frontend
+  "https://devpulse-kislai.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Cho phép request không có Origin
+      // Ví dụ: Postman, Swagger, server-to-server
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+
+    allowedHeaders: ["Content-Type", "Authorization"],
+
+    credentials: true,
+  }),
+);
